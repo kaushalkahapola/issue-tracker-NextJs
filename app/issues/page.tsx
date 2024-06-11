de11c0c7d0cc3637536e617b5dd05b7b5e8c0024
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-11 13:14:19 +0530

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuePage = () => {
   return (
-    <div>IssuePage</div>
-  )
+      <Link href='/issues/new'>
+        <Button>
+          Create Issue
+        </Button>
+      </Link>
+    )
 }
 
 export default IssuePage
