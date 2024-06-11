4422d602d54b0ef423d8c737b277ddbe6daf16e5
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-11 13:22:43 +0530

    created issues new page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { TextField , Button} from "@radix-ui/themes";
+import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title">
+        <TextField.Slot>
+        </TextField.Slot>
+      </TextField.Root>
+      <SimpleMDE placeholder="Description" />;
+      <Button>Submit new issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Theme accentColor="teal">
+        <Theme accentColor="teal">
           <NavBar />
-          {children}
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
